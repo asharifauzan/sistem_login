1956c6c9df760e09d859b41d8b87886254bc6f50
Fauzan Hibatullah Ashari <52328248+asharifauzan@users.noreply.github.com>
2020-07-01 02:06:50 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://sistem-login.netlify.app/';
+$config['base_url'] = 'http://sistem-login-fauzan.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
